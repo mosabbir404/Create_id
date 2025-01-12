def6a8736ce4baaee6c6ef66f932eaf1da61ceec
Md Mosabbir Hossain <bmads2025@gmail.com>
2025-01-12 11:58:06 +0600

    Add files via upload

---
+++ create.py
@@ -0,0 +1,230 @@
+import os,sys,re,time,json
+import requests,bs4,string
+import faker,fake_email,random
+from faker import Faker
+from fake_email import Email
+from bs4 import BeautifulSoup
+
+W = "\x1b[97m"
+G = "\x1b[38;5;46m"
+R = "\x1b[38;5;196m"
+X = f"{W}<{R}•{W}>"
+
+oks = []
+cps = []
+
+user_agents = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:132.0) Gecko/20100101 Firefox/132.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:129.0) Gecko/20100101 Firefox/129.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:127.0) Gecko/20100101 Firefox/127.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1",
+    "Mozilla/5.0 (iPad; CPU OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.1020.40 Safari/537.36 Edg/95.0.1020.40",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.1054.62 Safari/537.36 Edg/96.0.1054.62",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97. 0.4692.71 Safari/537.36 Edg/97.0.4692.71",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36 OPR/81.0.4196.31",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36 OPR/82.0.4227.58",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36 OPR/83.0.4254.27",
+    "Mozilla/5.0 (Linux; Android 10; Pixel 3 XL) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.74 Mobile Safari/537.36",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1",
+    "Mozilla/5.0 (Linux; Android 11; Samsung Galaxy S21) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Mobile Safari/537.36",
+    "Mozilla/5.0 (iPad; CPU OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1",
+]
+
+def ugenX():
+    return str(random.choice(user_agents))
+def fake_name():
+    first = Faker().first_name()
+    last = Faker().last_name()
+    return first,last
+
+def extractor(data):
+    try:
+        soup = BeautifulSoup(data,"html.parser")
+        data = {}
+        for inputs in soup.find_all("input"):
+            name = inputs.get("name")
+            value = inputs.get("value")
+            if name:
+                data[name] = value
+        return data
+    except Exception as e:
+        return {"error":str(e)}
+
+def GetEmail():
+    response = requests.post('https://api.internal.temp-mail.io/api/v3/email/new').json()
+    return response['email']
+
+def GetCode(email):
+    try:
+        response = requests.get(f'https://api.internal.temp-mail.io/api/v3/email/{email}/messages').text
+        code = re.search(r'FB-(\d+)', response).group(1)
+        return code
+    except:
+        return None
+
+def banner():
+    os.system("clear")
+    print(f"{W}<{R}•{W}> FACEBOOK AUTO ID CREATOR")
+    print(f"{W}<{R}•{W}> CODED :- {G}HADI ANHAF AIMAN")
+    print(f"{W}———————————————————————————————")
+
+def linex():
+    print(f"{W}———————————————————————————————")
+
+def main() -> None:
+    banner()
+    input(f"{X} PRESS ENTER TO START....")
+    linex()
+    for make in range(100):
+        ses = requests.Session()
+        response = ses.get(
+            url='https://touch.facebook.com/reg',
+            params={"_rdc":"1","_rdr":"","wtsid":"rdr_0t3qOXoIHbMS6isLw","refsrc":"deprecated"},
+        )
+        mts = ses.get("https://touch.facebook.com").text
+        m_ts = re.search(r'name="m_ts" value="(.*?)"',str(mts)).group(1)
+        formula = extractor(response.text)
+        email2 = GetEmail()
+        firstname,lastname = fake_name()
+        print(f"{X} NAME  - {G}{firstname} {lastname}")
+        print(f"{X} EMAIL - {G}{email2}")
+        payload = {
+            'ccp': "2",
+            'reg_instance': str(formula["reg_instance"]),
+            'submission_request': "true",
+            'helper': "",
+            'reg_impression_id': str(formula["reg_impression_id"]),
+            'ns': "1",
+            'zero_header_af_client': "",
+            'app_id': "103",
+            'logger_id': str(formula["logger_id"]),
+            'field_names[0]': "firstname",
+            'firstname': firstname,
+            'lastname': lastname,
+            'field_names[1]': "birthday_wrapper",
+            'birthday_day': str(random.randint(1,28)),
+            'birthday_month': str(random.randint(1,12)),
+            'birthday_year': str(random.randint(1992,2009)),
+            'age_step_input': "",
+            'did_use_age': "false",
+            'field_names[2]': "reg_email__",
+            'reg_email__': email2,
+            'field_names[3]': "sex",
+            'sex': "2",
+            'preferred_pronoun': "",
+            'custom_gender': "",
+            'field_names[4]': "reg_passwd__",
+            'name_suggest_elig': "false",
+            'was_shown_name_suggestions': "false",
+            'did_use_suggested_name': "false",
+            'use_custom_gender': "false",
+            'guid': "",
+            'pre_form_step': "",
+            'encpass': f"#PWD_BROWSER:{m_ts}:MRCODE@123",
+            'submit': "Sign Up",
+            'fb_dtsg': "NAcMC2x5X2VrJ7jhipS0eIpYv1zLRrDsb5y2wzau2bw3ipw88fbS_9A:0:0",
+            'jazoest': str(formula["jazoest"]),
+            'lsd': str(formula["lsd"]),
+            '__dyn': "1ZaaAG1mxu1oz-l0BBBzEnxG6U4a2i5U4e0C8dEc8uwcC4o2fwcW4o3Bw4Ewk9E4W0pKq0FE6S0x81vohw5Owk8aE36wqEd8dE2YwbK0iC1qw8W0k-0jG3qaw4kwbS1Lw9C0le0ue0QU",
+            '__csr': "",
+            '__req': "p",
+            '__fmt': "1",
+            '__a': "AYkiA9jnQluJEy73F8jWiQ3NTzmH7L6RFbnJ_SMT_duZcpo2yLDpuVXfU2doLhZ-H1lSX6ucxsegViw9lLO6uRx31-SpnBlUEDawD_8U7AY4kQ",
+            '__user': "0"
+        }
+        header1 = {
+            "Host":"m.facebook.com",
+            "Connection":"keep-alive",
+            "Upgrade-Insecure-Requests":"1",
+            "User-Agent":ugenX(),
+            "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "dnt":"1",
+            "X-Requested-With":"mark.via.gp",
+            "Sec-Fetch-Site":"none",
+            "Sec-Fetch-Mode":"navigate",
+            "Sec-Fetch-User":"?1",
+            "Sec-Fetch-Dest":"document",
+            "dpr":"1.75",
+            "viewport-width":"980",
+            "sec-ch-ua":"\"Android WebView\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
+            "sec-ch-ua-mobile":"?1",
+            "sec-ch-ua-platform":"\"Android\"",
+            "sec-ch-ua-platform-version":"\"\"",
+            "sec-ch-ua-model":"\"\"",
+            "sec-ch-ua-full-version-list":"",
+            "sec-ch-prefers-color-scheme":"dark",
+            "Accept-Encoding":"gzip, deflate, br, zstd",
+            "Accept-Language":"en-GB,en-US;q=0.9,en;q=0.8"
+        }
+        reg_url = "https://m.facebook.com/reg/submit/?privacy_mutation_token=eyJ0eXBlIjowLCJjcmVhdGlvbl90aW1lIjoxNzM0NDE0OTk2LCJjYWxsc2l0ZV9pZCI6OTA3OTI0NDAyOTQ4MDU4fQ%3D%3D&multi_step_form=1&skip_suma=0&shouldForceMTouch=1"
+        py_submit = ses.post(reg_url, data=payload, headers=header1)
+        if "c_user" in py_submit.cookies:
+            first_cok = ses.cookies.get_dict()
+            uid = str(first_cok["c_user"])
+            header2 = {
+                'authority': 'm.facebook.com',
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'accept-language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7',
+                'cache-control': 'max-age=0',
+                'dpr': '2',
+                'referer': 'https://m.facebook.com/login/save-device/',
+                'sec-ch-prefers-color-scheme': 'light',
+                'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="125", "Google Chrome";v="125"',
+                'sec-ch-ua-mobile': '?1',
+                'sec-ch-ua-platform': '"Android"',
+                'sec-fetch-dest': 'document',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'same-origin',
+                'sec-fetch-user': '?1',
+                'upgrade-insecure-requests': '1',
+                'user-agent': ugenX(),
+                'viewport-width': '980',      
+            }
+            params = {
+                'next': 'https://m.facebook.com/?deoia=1',
+                'soft': 'hjk',
+            }
+            con_sub = ses.get('https://x.facebook.com/confirmemail.php', params=params, headers=header2).text
+            valid = GetCode(email2)
+            if valid:
+                print(f"{X} LOGIN OTP - {G}{valid}")
+                cookie = (";").join([ "%s=%s" % (key,value) for key,value in ses.cookies.get_dict().items()])
+                try:
+                    data={
+                        'email':uid,
+                        'otp':valid,
+                        'cookie':cookie,
+                    }
+                    url = "https://mdraj.pythonanywhere.com/api/verify"
+                    sub = requests.post(url,data=data).json()
+                    if "Otp Confirmed" in sub:
+                        print(f"{X} SUCCESS - {G}{uid}|MRCODE@123")
+                        open("/sdcard/SUCCESS-OK-ID.txt","a").write(uid+"|MRCODE@123\n")
+                        linex()
+                    elif "Locked Account!" in sub:
+                        print(f"{X} \x1b[38;4;200mSUCCESSFULLY LOCKED ID")
+                        linex()
+                except Exception as e:pass
+            else:
+                print(f"{X} \x1b[38;5;206mSUCCESSFULLY DISABLED ID")
+                linex()
+        else:
+            print(f"{X} {R}CHECKPOINT ID")
+            linex()
+
+
+if __name__ == "__main__":
+    main()
